bc1928c50288657bf492f98e3e9a63718d7786ed
Subhajit Sahu <wolfram77@gmail.com>
2022-02-02 16:05:40 +0530

    :turtle: update name

--- main.rkt
+++ main.rkt
@@ -1,11 +1,11 @@
 #lang racket
 
-; (invert lst): List(N X N) -> List(N X N)
+; (list.reverse-pairs lst): List(N X N) -> List(N X N)
 ; returns a list with each 2-list reversed
 ; : size(lst)=0 -> ()
-; : else        -> ((lst[0,1] lst[0,0]) (invert lst[1..end]))
-(define invert
+; : else        -> ((lst[0,1] lst[0,0]) (list.reverse-pairs lst[1..end]))
+(define list.reverse-pairs
   (lambda (lst)
     (if (null? lst)
         (list)
-        (cons (list (cadar lst) (caar lst)) (invert (cdr lst))))))
+        (cons (list (cadar lst) (caar lst)) (list.reverse-pairs (cdr lst))))))
